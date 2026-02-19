aa9d9e3a571a49c53e842adcc41411abbd59cd72
nagi-kinjou-k25f-debug <nagi-kinjou-k25f@stu.kbc.ac.jp>
2026-02-19 15:37:21 +0900

    タイトル画面のボタン追加
    タイトル画面にゲーム名、スタート、ランキング、終了の4つを追加しました

--- Scenes/Title.cpp
+++ Scenes/Title.cpp
@@ -4,6 +4,8 @@
 
 #include <string>
 
+int cursor_number = 0;
+
 TitleScene::TitleScene()
 {
 	next_scene = eSceneType::eTitle;
@@ -30,6 +32,35 @@ eSceneType TitleScene::Update()
 		return eSceneType::eInGame;
 	}
 
+	if (pad_input->GetButtonInputState(XINPUT_BUTTON_DPAD_UP) == ePadInputState::ePress)
+	{
+		cursor_number--;
+		if (cursor_number < 0)
+		{
+			cursor_number = 2;
+		}
+	}
+	if (pad_input->GetButtonInputState(XINPUT_BUTTON_DPAD_DOWN) == ePadInputState::ePress)
+	{
+		cursor_number++;
+		if (cursor_number > 2)
+		{
+			cursor_number = 0;
+		}
+	}
+
+	if (pad_input->GetButtonInputState(XINPUT_BUTTON_A) == ePadInputState::ePress)
+	{
+		switch (cursor_number)
+		{
+		case 0:
+			return eSceneType::eTitle;
+		case 1:
+			return eSceneType::eRanking;
+		case 2:
+			return eSceneType::eEnd;
+		}
+	}
 	Draw();
 	return eSceneType::eTitle;
 }
@@ -38,7 +69,11 @@ eSceneType TitleScene::Update()
 void TitleScene::Draw() const
 {
 	DrawFormatString(120, 140, GetColor(255, 255, 0), "タイトルシーンです");
-	
+	DrawFormatString(560, 240, GetColor(255, 255, 0), "ゲーム名");
+	DrawFormatString(560, 440, GetColor(255, 255, 0), "ゲームスタート");
+	DrawFormatString(560, 460, GetColor(255, 255, 0), "ランキング");
+	DrawFormatString(560, 480, GetColor(255, 255, 0), "ゲーム終了");
+	DrawFormatString(540, 440, GetColor(255, 255, 0), "→");
 }
 
 void TitleScene::Finalize()
