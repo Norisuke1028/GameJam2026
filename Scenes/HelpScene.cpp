6cbf4ce013085711bee91b1c5b878fd2b6fe279f
keito-yamazato-k25f-collab <keito-yamazato-k25f@stu.kbc.ac.jp>
2026-02-20 14:42:19 +0900

    HelpSceneを作りました

--- Scenes/HelpScene.cpp
+++ Scenes/HelpScene.cpp
@@ -1,1 +1,52 @@
 #include "HelpScene.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+#include <string>
+
+HelpScene::HelpScene()
+{
+	next_scene = eSceneType::eHelp;
+}
+
+HelpScene::~HelpScene()
+{
+
+}
+
+void HelpScene::Initialize()
+{
+	printf("ヘルプシーン");
+}
+
+eSceneType HelpScene::Update()
+{
+	// 入力情報を取得
+	InputControl* pad_input = InputControl::GetInstance();
+
+	// タイトル画面に遷移する
+	if (pad_input->GetButtonInputState(XINPUT_BUTTON_B) == ePadInputState::ePress)
+	{
+		return eSceneType::eTitle;
+	}
+
+	Draw();
+	return eSceneType::eHelp;
+}
+
+// 描画処理
+void HelpScene::Draw() const
+{
+	DrawFormatString(120, 140, GetColor(255, 255, 0), "ヘルプシーン");
+
+}
+
+void HelpScene::Finalize()
+{
+
+}
+
+eSceneType HelpScene::GetNowSceneType() const
+{
+	return eSceneType::eHelp;
+}
