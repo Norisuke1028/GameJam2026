637d814c82f2792a8dabdc025326719c094bc8b2
keito-yamazato-k25f-collab <keito-yamazato-k25f@stu.kbc.ac.jp>
2026-02-20 14:39:28 +0900

    enemyを描画と動く処理を追加

--- Objects/Enemy/Enemy.h
+++ Objects/Enemy/Enemy.h
@@ -1,5 +1,24 @@
 #pragma once
-class Enemy
-{
-};
 
+#include "../GameObject.h"
+#include <vector>
+
+
+
+class Enemy : public GameObject
+{
+public:
+	Enemy();
+	virtual ~Enemy();
+
+	void Initialize() override;
+	void Update() override;
+	void Draw(const Vector2D& screen_offset) const override;
+	void Finalize() override;
+
+	//ˆÊ’uî•ñæ“¾ˆ—
+	const Vector2D& GetLocation() const;
+	//ˆÊ’uî•ñæ“¾ˆ—
+	const Vector2D& GetVelocity() const;
+
+};
