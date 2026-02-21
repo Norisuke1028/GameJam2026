62d13eba8a8fe6a57039bec76c658295e7333bb3
nagi-kinjou-k25f-debug <nagi-kinjou-k25f@stu.kbc.ac.jp>
2026-02-21 10:45:52 +0900

    タイトル画面のアニメーション追加
    タイトル画面の項目の画像を追加しました

--- Scenes/Title.cpp
+++ Scenes/Title.cpp
@@ -5,18 +5,30 @@
 #include <string>
 
 int cursor_number = 0;
+int a = 0;
 int title_image;
 int start_image;
 int shuryo_image;
+int help_image;
 int cursor_image;
+int start1_image;
+int shuryo1_image;
+int help1_image;
 
 TitleScene::TitleScene()
 {
 	next_scene = eSceneType::eTitle;
+	// 画像の読み込み
 	title_image = LoadGraph("Resource/image/Title/title.png");
 	start_image = LoadGraph("Resource/image/Title/startmae.png");
 	shuryo_image = LoadGraph("Resource/image/Title/shuryoumae.png");
+	help_image = LoadGraph("Resource/image/Title/helpmae.png");
+
 	cursor_image = LoadGraph("Resource/image/Title/yajirusi.png");
+
+	start1_image = LoadGraph("Resource/image/Title/startgo.png");
+	shuryo1_image = LoadGraph("Resource/image/Title/shuryougo.png");
+	help1_image = LoadGraph("Resource/image/Title/helpato.png");
 }
 
 TitleScene::~TitleScene()
@@ -40,6 +52,7 @@ eSceneType TitleScene::Update(const float& delta_second)
 		return eSceneType::eInGame;
 	}
 
+	// カーソルの移動
 	if (pad_input->GetButtonInputState(XINPUT_BUTTON_DPAD_UP) == ePadInputState::ePress)
 	{
 		cursor_number--;
@@ -57,12 +70,15 @@ eSceneType TitleScene::Update(const float& delta_second)
 		}
 	}
 
+	// 決定する
 	if (pad_input->GetButtonInputState(XINPUT_BUTTON_A) == ePadInputState::ePress)
 	{
+		a += 1;
+		// カーソルの位置に応じて遷移先のシーンを変更する
 		switch (cursor_number)
 		{
 		case 0:
-			return eSceneType::eTitle;
+			return eSceneType::eInGame;
 		case 1:
 			return eSceneType::eHelp;
 		case 2:
@@ -76,19 +92,48 @@ eSceneType TitleScene::Update(const float& delta_second)
 //描画処理
 void TitleScene::Draw() const
 {
-	/*DrawFormatString(120, 140, GetColor(255, 255, 0), "タイトルシーンです");*/
-	/*DrawFormatString(560, 240, GetColor(255, 255, 0), "ゲーム名");*/
-	/*DrawFormatString(60, 440, GetColor(255, 255, 0), "ゲームスタート");*/
-	/*DrawFormatString(60, 460, GetColor(255, 255, 0), "ヘルプ");
-	DrawFormatString(60, 480, GetColor(255, 255, 0), "終了");*/
 	DrawGraph(0, 0, title_image, TRUE);
 	DrawGraph(90, 240, start_image, TRUE);
+	DrawGraph(115, 350, help_image, TRUE);
 	DrawGraph(90, 330, shuryo_image, TRUE);
 	
-	/*DrawFormatString(540, 440, GetColor(255, 255, 0), "→");*/
-	int cursor = cursor_number * 100;
-	/*DrawString(40, 440 + cursor, "→", GetColor(255, 255, 0));*/
-	DrawGraph(0, 300 + cursor, cursor_image, TRUE);
+	int cursor = cursor_number * 100; // カーソルの位置を計算
+	DrawGraph(0, 300 + cursor, cursor_image, TRUE); // カーソルを描画
+
+	if (cursor_number == 0 /*&& a == 1*/)
+	{
+		DrawGraph(90, 240, start1_image, TRUE);
+		
+	}
+	if (cursor_number == 1 /*&& a == 1*/)
+	{
+		DrawGraph(115, 350, help1_image, TRUE);
+		
+	}
+	if (cursor_number == 2 /*&& a == 1*/)
+	{
+		DrawGraph(90, 330, shuryo1_image, TRUE);
+		
+	}
+	//switch (cursor_number)
+	//{
+	//case 0:
+	//	DrawGraph(90, 240, start1_image, TRUE);
+	//	break;
+	//case 1:
+	//	DrawGraph(115, 350, help1_image, TRUE);
+	//	break;
+	//case 2:
+	//	DrawGraph(90, 330, shuryo1_image, TRUE);
+	//	break;
+	//}
+
+	//if (a == 1)
+	//{
+	//	
+	//	WaitTimer(1000); // 1秒待機
+	//	a--;
+	//}
 }
 
 void TitleScene::Finalize()
